ed7720ee0fc6f32631821f747807835634e43b82
lschaack <luke.schaack@gmail.com>
2018-09-23 14:38:45 -0700

    add semi-random prediction based on normalized probabilities of softmax output, add support for char- or word-level models based on config, improve accuracy reporting

--- grammar-pyml/general_lm.py
+++ grammar-pyml/general_lm.py
@@ -20,6 +20,7 @@ class MetaModel(object):
         self.id_to_word = id_to_word
         self.config = configuration
         # bit of a hack--save important params in case they change in the global class
+        self.n_possibilities = configuration.n_possibilities
         self.hidden_size = configuration.hidden_size
         self.num_layers = configuration.num_layers
         self.init_scale = configuration.init_scale
@@ -43,8 +44,9 @@ class Model(object):
             tf.int32, [self.batch_size, self.seq_length])
         self.Y_true = tf.placeholder(
             tf.int32, [self.batch_size, self.seq_length])
-        inputs = self._get_inputs(self.X, self.mode)
-        if self.mode == 'word' and is_training and config.keep_prob < 1:
+        inputs = self._get_inputs(self.X, config)
+        
+        if is_training and config.keep_prob < 1:
             inputs = tf.nn.dropout(inputs, config.keep_prob)
         
         # set up the state storage / extraction
@@ -83,7 +85,20 @@ class Model(object):
 
         # get the prediction accuracy
         self.softmax_out = tf.nn.softmax(tf.reshape(logits, [-1, self.vocab_size]))
-        self.predict = tf.cast(tf.argmax(self.softmax_out, axis=1), tf.int32)
+
+        if is_training:
+            self.predict = tf.cast(tf.argmax(self.softmax_out, axis=1), tf.int32)
+        else:
+            # use normalized probs of top k outputs to add some randomness to the output
+            _, possibilities = tf.nn.top_k(self.softmax_out, k=config.n_possibilities, sorted=True)
+            choice_reduce = lambda x: tf.py_func(np.random.choice,
+                                                 [x, 1, True, tf.divide(
+                                                     tf.cast(x, tf.float32),
+                                                     tf.norm(tf.cast(x, tf.float32),ord=1))],
+                                                 tf.int32)
+            choices = tf.map_fn(choice_reduce, possibilities)
+            self.predict = choices
+
         correct_prediction = tf.equal(self.predict, tf.reshape(self.Y_true, [-1]))
         self.accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
@@ -105,7 +120,7 @@ class Model(object):
         # helper function to avoid variable confusion
         # create an LSTM cell to be unrolled, add dropout wrapper if training
         def make_cell():
-            cell = tf.contrib.rnn.LSTMCell(hidden_size, forget_bias=0.0)
+            cell = tf.contrib.rnn.LSTMCell(hidden_size, forget_bias=1.0)
             if is_training and config.keep_prob < 1:
                 return tf.contrib.rnn.DropoutWrapper(cell, output_keep_prob=config.keep_prob)
             else:
@@ -119,12 +134,12 @@ class Model(object):
         output = tf.reshape(output, [-1, hidden_size])
         return output, state
 
-    def _get_inputs(self, X, mode):
-        if mode == 'char':
+    def _get_inputs(self, X, config):
+        if config.mode == 'char':
             return tf.one_hot(self.X, depth=self.vocab_size,
                               on_value=1.0, off_value=0.0,
                               axis=-1, dtype=tf.float32)
-        elif mode == 'word': # intuitively, embeddings only seem to make sense on a word-level model
+        elif config.mode == 'word': # intuitively, embeddings only seem to make sense on a word-level model
             with tf.device("/cpu:0"):
             	embedding = tf.Variable(tf.random_uniform([self.vocab_size, self.hidden_size], 
                                         -config.init_scale, config.init_scale))
@@ -139,10 +154,11 @@ class Model(object):
 def run_epoch(session, lm, config, processed, epoch_size, prints_per_epoch=10, shuffle_iter=None):
     current_state = np.zeros((config.num_layers, 2, config.batch_size, lm.hidden_size))
     print_iter = max(1, epoch_size // prints_per_epoch) # ensure no modulo by zero
+    total_prints = 0 # TODO: hack-y. do better.
     total_acc = 0
     print("epoch size:", epoch_size)
     curr_time = dt.datetime.now()
-    i = 0 # TODO: something better
+    i = 0 # TODO: maybe something better?
     # shuffle the data in blocks for better generalization
     for (X, Y) in processed.doc_slice(config.batch_size, config.seq_length, batches_to_reset=shuffle_iter):
         # below per theblog.github.io/post/character-language-model-lstm-tensorflow/
@@ -158,8 +174,10 @@ def run_epoch(session, lm, config, processed, epoch_size, prints_per_epoch=10, s
                                                     feed_dict={lm.init_state: current_state, lm.X: X, lm.Y_true: Y})
             print("Step {}, cost: {:.3f}, accuracy: {:.3f}, Seconds per step: {:.3f}".format(i, cost, acc, seconds))
             total_acc += acc
+            total_prints += 1
         i += 1
-    return total_acc / np.ceil(epoch_size / print_iter) # maybe there should be a ceiling function here?
+    # Seems like there should be a better fix for below
+    return total_acc / total_prints
 
 def train(model, model_name, processed, config=None, start_epoch=0):
     init_op = tf.global_variables_initializer()
@@ -181,6 +199,10 @@ def train(model, model_name, processed, config=None, start_epoch=0):
             else:
                 raise ValueError('Could not find a model path for epoch ' + str(start_epoch))
         
+        # ensure learning rate is properly decayed on resume
+        learning_rate = config.learning_rate * config.lr_decay ** max((start_epoch - 1) // config.max_epoch, 0)
+        ###############################################
+
         ### save info for later generation ###
         meta = MetaModel(processed.word_to_id, processed.id_to_word, config)
         
@@ -194,15 +216,18 @@ def train(model, model_name, processed, config=None, start_epoch=0):
 
         coord = tf.train.Coordinator()
         threads = tf.train.start_queue_runners(coord=coord)
-        model.assign_lr(sess, config.learning_rate)
 
         for epoch in range(start_epoch, config.max_max_epoch):
+            model.assign_lr(sess, config.learning_rate)
             print("{s:#^80}\n".format(s=' Entering epoch {} '.format(epoch)))
-            # TODO: hook up shuffle_iter much better
-            epoch_acc = run_epoch(sess, model, config, processed, epoch_size, prints_per_epoch=10, shuffle_iter=20)
+            epoch_acc = run_epoch(sess, model, config, processed, epoch_size,
+                                  prints_per_epoch=10,
+                                  shuffle_iter=config.shuffle_iter)
             print("\n{s:#^80}\n".format(s=" Epoch accuracy {:.3f} ".format(epoch_acc))) # TODO: change nested .formats
             # save a model checkpoint
             saver.save(sess, model_path, global_step=epoch)
+            if epoch % config.max_epoch == 0:
+                learning_rate *= config.lr_decay
             # TODO: get validation accuracy here at each checkpoint
         coord.request_stop()
         coord.join(threads)
@@ -232,8 +257,11 @@ if __name__ == '__main__':
     basepath = os.path.splitext(os.path.basename(args.filepath))[0]
 
     # load in data
-    processed = reader.DocReader(args.filepath)
+    print("getting config")
     config = get_config(args.config)
+    print("got config, processing...")
+    processed = reader.DocReader(args.filepath, config.mode)
+    print("Sanity check: {}...".format(processed.data[:50]))
     ########## Train ##########
     lm = Model(config, processed.vocab_size, is_training=True)
     train(lm, basepath, processed, config, start_epoch=args.resume)
