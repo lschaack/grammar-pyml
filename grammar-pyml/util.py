f2aa8b879e7da150315575709ecb56e44737925e
lschaack <luke.schaack@gmail.com>
2018-09-27 11:34:40 -0700

    fix issue with newline spacing, remove need for os import

--- grammar-pyml/util.py
+++ grammar-pyml/util.py
@@ -1,5 +1,3 @@
-import os
-
 # Draws a box around a nearly-arbitrary input text with optional title
 #  currently text with no spaces is unsupported
 def box(text, title=None, alignment='center', width=80):
@@ -14,18 +12,16 @@ def box(text, title=None, alignment='center', width=80):
     mid_lines = []
 
     alignment_char = get_alignment_char(alignment)
+    text = text.replace('\t', '    ') # set uniform tab width for advance knowledge
 
     line_text = ''
     curr_len = 0
     for index in range(len(text)): # loop through string char by char
+        line_text += text[index]
         # break on newline or string reaching max width of line
         if curr_len < stride_len and text[index] != '\n':
-            line_text += text[index] # might be slow
             curr_len += 1
         else:
-            # implicitly handle special chars
-            line_text = line_text.strip().replace('\t', '    ')
-            line_text += text[index]
             line_text, leftover = split_last_full_word(line_text)
             mid_lines.append(line_from_text(line_text, alignment_char, stride_len))
             # only add current char if not a newline
@@ -36,7 +32,7 @@ def box(text, title=None, alignment='center', width=80):
         mid_lines.append(line_from_text(line_text, alignment_char, stride_len))
 
     full_box = top_line + ''.join(mid_lines) + bottom_line
-    return full_box.replace('<nlc>', os.linesep)
+    return full_box.replace('<nlc>', '\n')
 
 # Gets the proper character from the alignment param, throws ValueError if not found
 def get_alignment_char(alignment):
@@ -51,18 +47,17 @@ def get_alignment_char(alignment):
 
 # Handles the application of settings like alignment and line width
 def line_from_text(line_text, alignment_char, stride_len):
-    mid_line = '| {:<arc><wid>} |<nlc>'
+    mid_line = '| {0:<arc><wid>} |<nlc>'
     mid_line = mid_line.replace('<arc>', alignment_char) # ensure proper alignment
     mid_line = mid_line.replace('<wid>', str(stride_len)) # ensure proper width
     return mid_line.format(line_text)
 
 # Splits the line on the last full word if necessary
 def split_last_full_word(line_text):
-    if line_text[-1] != '\n':
-        # try/except quicker and cheaper than conditional since lines w/o ' ' are so rare
-        try:
+    if line_text[-1] != '\n': # negation first b/c far more common
+        try: # quicker and cheaper than conditional since lines w/o ' ' are so rare
             final_space_index = line_text.rindex(' ')
-            return line_text[:final_space_index], line_text[final_space_index:]
+            return line_text[:final_space_index], line_text[final_space_index + 1:]
         except ValueError:
             return line_text, ''
     else:
@@ -112,19 +107,19 @@ From her at April are, ha! must not come?""",
     "Excerpt from Shakespeare, right aligned",
     alignment="right"))
 
-    print(box("""It was in one of the vast and gloomy chambers of this remaining tower that I, Antoine, last of the unhappy and accursed Counts de C-, first saw the light of day, ninety long years ago. Within these walls and amongst the dark and shadowy forests, the wild ravines and grottos of the hillside below, were spent the first years of my troubled life. My parents I never knew. My father had been killed at the age of thirty-two, a month before I was born, by the fall of a stone somehow dislodged from one of the deserted parapets of the castle. And my mother having died at my birth, my care and education devolved solely upon one remaining servitor, an old and trusted man of considerable intelligence, whose name I remember as Pierre. I was an only child and the lack of companionship which this fact entailed upon me was augmented by the strange care exercised by my aged guardian, in excluding me from the society of the peasant children whose abodes were scattered here and there upon the plains that surround the base of the hill. At that time, Pierre said that this restriction was imposed upon me because my noble birth placed me above association with such plebeian company. Now I know that its real object was to keep from my ears the idle tales of the dread curse upon our line that were nightly told and magnified by the simple tenantry as they conversed in hushed accents in the glow of their cottage hearths.
+    print(box("""   It was in one of the vast and gloomy chambers of this remaining tower that I, Antoine, last of the unhappy and accursed Counts de C-, first saw the light of day, ninety long years ago. Within these walls and amongst the dark and shadowy forests, the wild ravines and grottos of the hillside below, were spent the first years of my troubled life. My parents I never knew. My father had been killed at the age of thirty-two, a month before I was born, by the fall of a stone somehow dislodged from one of the deserted parapets of the castle. And my mother having died at my birth, my care and education devolved solely upon one remaining servitor, an old and trusted man of considerable intelligence, whose name I remember as Pierre. I was an only child and the lack of companionship which this fact entailed upon me was augmented by the strange care exercised by my aged guardian, in excluding me from the society of the peasant children whose abodes were scattered here and there upon the plains that surround the base of the hill. At that time, Pierre said that this restriction was imposed upon me because my noble birth placed me above association with such plebeian company. Now I know that its real object was to keep from my ears the idle tales of the dread curse upon our line that were nightly told and magnified by the simple tenantry as they conversed in hushed accents in the glow of their cottage hearths.
 
     Thus isolated, and thrown upon my own resources, I spent the hours of my childhood in poring over the ancient tomes that filled the shadow haunted library of the chateau, and in roaming without aim or purpose through the perpetual dust of the spectral wood that clothes the side of the hill near its foot. It was perhaps an effect of such surroundings that my mind early acquired a shade of melancholy. Those studies and pursuits which partake of the dark and occult in nature most strongly claimed my attention.""", 
     "Excerpt from Lovecraft, left aligned",
     alignment="left"))
 
-    print(box("""It was in one of the vast and gloomy chambers of this remaining tower that I, Antoine, last of the unhappy and accursed Counts de C-, first saw the light of day, ninety long years ago. Within these walls and amongst the dark and shadowy forests, the wild ravines and grottos of the hillside below, were spent the first years of my troubled life. My parents I never knew. My father had been killed at the age of thirty-two, a month before I was born, by the fall of a stone somehow dislodged from one of the deserted parapets of the castle. And my mother having died at my birth, my care and education devolved solely upon one remaining servitor, an old and trusted man of considerable intelligence, whose name I remember as Pierre. I was an only child and the lack of companionship which this fact entailed upon me was augmented by the strange care exercised by my aged guardian, in excluding me from the society of the peasant children whose abodes were scattered here and there upon the plains that surround the base of the hill. At that time, Pierre said that this restriction was imposed upon me because my noble birth placed me above association with such plebeian company. Now I know that its real object was to keep from my ears the idle tales of the dread curse upon our line that were nightly told and magnified by the simple tenantry as they conversed in hushed accents in the glow of their cottage hearths.
+    print(box("""   It was in one of the vast and gloomy chambers of this remaining tower that I, Antoine, last of the unhappy and accursed Counts de C-, first saw the light of day, ninety long years ago. Within these walls and amongst the dark and shadowy forests, the wild ravines and grottos of the hillside below, were spent the first years of my troubled life. My parents I never knew. My father had been killed at the age of thirty-two, a month before I was born, by the fall of a stone somehow dislodged from one of the deserted parapets of the castle. And my mother having died at my birth, my care and education devolved solely upon one remaining servitor, an old and trusted man of considerable intelligence, whose name I remember as Pierre. I was an only child and the lack of companionship which this fact entailed upon me was augmented by the strange care exercised by my aged guardian, in excluding me from the society of the peasant children whose abodes were scattered here and there upon the plains that surround the base of the hill. At that time, Pierre said that this restriction was imposed upon me because my noble birth placed me above association with such plebeian company. Now I know that its real object was to keep from my ears the idle tales of the dread curse upon our line that were nightly told and magnified by the simple tenantry as they conversed in hushed accents in the glow of their cottage hearths.
 
     Thus isolated, and thrown upon my own resources, I spent the hours of my childhood in poring over the ancient tomes that filled the shadow haunted library of the chateau, and in roaming without aim or purpose through the perpetual dust of the spectral wood that clothes the side of the hill near its foot. It was perhaps an effect of such surroundings that my mind early acquired a shade of melancholy. Those studies and pursuits which partake of the dark and occult in nature most strongly claimed my attention.""", 
     "Excerpt from Lovecraft, center aligned",
     alignment="center"))
 
-    print(box("""It was in one of the vast and gloomy chambers of this remaining tower that I, Antoine, last of the unhappy and accursed Counts de C-, first saw the light of day, ninety long years ago. Within these walls and amongst the dark and shadowy forests, the wild ravines and grottos of the hillside below, were spent the first years of my troubled life. My parents I never knew. My father had been killed at the age of thirty-two, a month before I was born, by the fall of a stone somehow dislodged from one of the deserted parapets of the castle. And my mother having died at my birth, my care and education devolved solely upon one remaining servitor, an old and trusted man of considerable intelligence, whose name I remember as Pierre. I was an only child and the lack of companionship which this fact entailed upon me was augmented by the strange care exercised by my aged guardian, in excluding me from the society of the peasant children whose abodes were scattered here and there upon the plains that surround the base of the hill. At that time, Pierre said that this restriction was imposed upon me because my noble birth placed me above association with such plebeian company. Now I know that its real object was to keep from my ears the idle tales of the dread curse upon our line that were nightly told and magnified by the simple tenantry as they conversed in hushed accents in the glow of their cottage hearths.
+    print(box("""   It was in one of the vast and gloomy chambers of this remaining tower that I, Antoine, last of the unhappy and accursed Counts de C-, first saw the light of day, ninety long years ago. Within these walls and amongst the dark and shadowy forests, the wild ravines and grottos of the hillside below, were spent the first years of my troubled life. My parents I never knew. My father had been killed at the age of thirty-two, a month before I was born, by the fall of a stone somehow dislodged from one of the deserted parapets of the castle. And my mother having died at my birth, my care and education devolved solely upon one remaining servitor, an old and trusted man of considerable intelligence, whose name I remember as Pierre. I was an only child and the lack of companionship which this fact entailed upon me was augmented by the strange care exercised by my aged guardian, in excluding me from the society of the peasant children whose abodes were scattered here and there upon the plains that surround the base of the hill. At that time, Pierre said that this restriction was imposed upon me because my noble birth placed me above association with such plebeian company. Now I know that its real object was to keep from my ears the idle tales of the dread curse upon our line that were nightly told and magnified by the simple tenantry as they conversed in hushed accents in the glow of their cottage hearths.
 
     Thus isolated, and thrown upon my own resources, I spent the hours of my childhood in poring over the ancient tomes that filled the shadow haunted library of the chateau, and in roaming without aim or purpose through the perpetual dust of the spectral wood that clothes the side of the hill near its foot. It was perhaps an effect of such surroundings that my mind early acquired a shade of melancholy. Those studies and pursuits which partake of the dark and occult in nature most strongly claimed my attention.""", 
     "Excerpt from Lovecraft, right aligned",
